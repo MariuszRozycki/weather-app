79bfdacb9f9e15193b342d5cb373a374526267f1
MariuszRozycki <mariansc83@icloud.com>
2024-03-24 15:30:12 +0100

    Add itemDetails (modal) after click in single item

--- src/components/WeatherItem.js
+++ src/components/WeatherItem.js
@@ -2,16 +2,18 @@ import "./Weather.css";
 
 const WeatherItem = (props) => {
   return (
-    <div className="weatherItem">
-      <h2>{props.weatherItem.stacja}</h2>
-      <p>Temp: {props.weatherItem.temperatura}°C. </p>
-      <p>
-        Pressure:{" "}
-        {props.weatherItem.cisnienie
-          ? props.weatherItem.cisnienie + " hpa"
-          : "No data"}
-      </p>
-    </div>
+    <>
+      <div className="weatherItem" onClick={props.onClick}>
+        <h2>{props.weatherItem.stacja}</h2>
+        <p>Temp: {props.weatherItem.temperatura}°C. </p>
+        <p>
+          Pressure:{" "}
+          {props.weatherItem.cisnienie
+            ? props.weatherItem.cisnienie + " hpa"
+            : "No data"}
+        </p>
+      </div>
+    </>
   );
 };
 
--- src/components/WeatherItemDetails.js
+++ src/components/WeatherItemDetails.js
@@ -1,3 +1,33 @@
-const WeatherItemDetails = () => {
-  return <div>Details</div>;
+import "./WeatherItemDetails.css";
+
+const WeatherItemDetails = (props) => {
+  console.log(props);
+  if (!props.item) return null;
+
+  return (
+    <div onClick={props.onClose} ref={props.modalRef} className="modal">
+      <div className="modal-content">
+        <button
+          className="closeModal-btn"
+          ref={props.buttonRef}
+          onClick={(e) => props.onClose}
+        >
+          X
+        </button>
+        <div className="modal-content--data">
+          <h2>{props.item.stacja}</h2>
+          <p>Pressure: {props.item.cisnienie} hpa</p>
+          <p>Measure date: {props.item.data_pomiaru} </p>
+          <p>Time of measure: {props.item.godzina_pomiaru}:00 hour</p>
+          <p>Wind direction: {props.item.kierunek_wiatru}° </p>
+          <p>Wind speed: {props.item.predkosc_wiatru} m/s </p>
+          <p>Total rainfall: {props.item.suma_opadu} mm </p>
+          <p>Temperature: {props.item.temperatura}°C </p>
+          <p>Relative humidity: {props.item.wilgotnosc_wzgledna}% </p>
+        </div>
+      </div>
+    </div>
+  );
 };
+
+export default WeatherItemDetails;
